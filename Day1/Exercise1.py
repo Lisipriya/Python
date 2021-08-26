c3634d62134ee969592d1a37ededed8a599e8353
Lisipriya <aplisipriya1998@gmail.com>
2021-08-26 15:38:00 +0530

    Exercise1

---
+++ Day1/Exercise1.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+#Day 1-1 Exercise
+print('Day 1 - Python Print Function')
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+
+
+
+
+
+
+
+
